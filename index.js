e82ee99f6667316bf55273acce5f86182827aaea
Seweje Damilola John <sewejed@gmail.com>
2023-12-21 17:26:42 +0100

    updated js file

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const h1 = document.querySelector('h1');
 const body = document.querySelector('body');
 const button = document.querySelector('button');
-h1.innerText = 'Hello Javascript';
+h1.innerText = 'Hello Javascript!!!!';
 
 button.addEventListener('click', () => {
     body.style.backgroundColor = 'black';
