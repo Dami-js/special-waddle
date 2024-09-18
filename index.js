880bea025469440e277baba3040929c9556df64e
Seweje Damilola John <sewejed@gmail.com>
2024-09-18 20:30:38 +0000

    update app to node app

--- index.js
+++
@@ -1,9 +0,0 @@
-const h1 = document.querySelector('h1');
-const body = document.querySelector('body');
-const button = document.querySelector('button');
-h1.innerText = 'Hello Javascript!!!!';
-
-button.addEventListener('click', () => {
-    body.style.backgroundColor = 'black';
-    body.style.color = 'white';
-})
